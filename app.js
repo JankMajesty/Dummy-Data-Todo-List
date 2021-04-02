ed2b85c17e5f272fe425ef16578fb6b8797c0c0a
JankMajesty <lmcgill36@gmail.com>
2021-04-01 20:21:45 -0500

    pushing javascript edits

--- app.js
+++ app.js
@@ -1,44 +1,46 @@
-
-   let arrayOfTodos = [
-    {
-    "userId": 14,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
+let arrayOfTodos = [
   {
-    "userId": 20,
-    "id": 2,
-    "title": "delectus aut autem",
-    "completed": false
-  }]
+  "userId": 14,
+  "id": 1,
+  "title": "delectus aut autem",
+  "completed": false
+},
+{
+  "userId": 20,
+  "id": 2,
+  "title": "delectus aut autem",
+  "completed": false
+}]
 
-  const fetchTodos = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then( (response) => response.json())
-    .then( (json) => arrayOfTodos = json)
-    console.log('arrayOfTodos', arrayOfTodos)
+const fetchTodos = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then( (response) => response.json())
+  .then( (json) => arrayOfTodos = json)
+  console.log('arrayOfTodos:', arrayOfTodos)
+}
+
+const logTodos = () => {
+  console.log(arrayOfTodos)
+}
+
+const populateTodos = () => {
+  // get ol tag to insert list items
+  const ol = document.getElementById('todo-list');
+
+  for (let index = 0; index < arrayOfTodos.length; index++) {
+    // create a new div element
+    const li = document.createElement("li");
+
+    // get title from object
+    const title = arrayOfTodos[index].title
+
+    // put title in the li element
+    const listItemText = document.createTextNode(title);
+
+    // add textNode to list element
+    li.appendChild(listItemText)
+    // add list item to ol element in DOM
+    // document.body.insertBefore(li, ol);
+    ol.appendChild(li)
   }
-
-  const logTodos = () => {
-    console.log(arrayOfTodos)
-  }
-
-  const populateTodos = () => {
-    // get ol tag to insert list items
-    const ol = document.getElementById('todo-list');
-
-    for (let index = 0; index < array.length; index++) {
-      const li = document.createElement("li");
-      const title = arrayOfTodos[index].title
-      const listItemText = document.createTextNode(title);
-      li.appendChild(listItemText)
-      document.body.insertBefore(li,ol);
-      
-      console.log('element:' element)
-      
-    }
-    //create and element <li></li>
-    //get parent element <ol></ol>
-    // 
-  }
+}
