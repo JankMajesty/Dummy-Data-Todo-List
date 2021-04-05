26203c7b049d85c84b4ac7445385b1d00b2dc433
JankMajesty <lmcgill36@gmail.com>
2021-04-05 17:10:40 -0500

    updating html and js for filter branch

--- app.js
+++ app.js
@@ -1,46 +1,83 @@
-let arrayOfTodos = [
-  {
-  "userId": 14,
-  "id": 1,
-  "title": "delectus aut autem",
-  "completed": false
-},
-{
-  "userId": 20,
-  "id": 2,
-  "title": "delectus aut autem",
-  "completed": false
-}]
+// let arrayOfTodos = [
+//   {
+//       "userId": 14,
+//       "id": 1,
+//       "title": "delectus aut autem",
+//       "completed": false
+//   },
+//   {
+//       "userId": 20,
+//       "id": 2,
+//       "title": "delectus aut autem",
+//       "completed": false
+//   }]
+
+let filteredList = ""
 
 const fetchTodos = () => {
   fetch('https://jsonplaceholder.typicode.com/todos')
-  .then( (response) => response.json())
-  .then( (json) => arrayOfTodos = json)
-  console.log('arrayOfTodos:', arrayOfTodos)
-}
+      .then((response) => response.json())
+      .then((json) => arrayOfTodos = json)
+      .then((json) => filteredList = json)
+} 
 
 const logTodos = () => {
   console.log(arrayOfTodos)
 }
+let todoList = document.getElementById('todo-list')
 
-const populateTodos = () => {
-  // get ol tag to insert list items
-  const ol = document.getElementById('todo-list');
+const populateTodos = (arrayOfTodos) => {
+  document.getElementById('todo-list').innerHTML = ""
+  arrayOfTodos.forEach(function (todo) {
 
-  for (let index = 0; index < arrayOfTodos.length; index++) {
-    // create a new div element
-    const li = document.createElement("li");
+      let li = document.createElement('li');
+      let doneButton = document.createElement('button');
+      li.textContent = todo.title;
+      todoList.appendChild(li);
+      
+      if (todo.completed == false) {
+          li.style.color = "red";
+          li.style.textDecoration = "none";
+          li.appendChild(doneButton)
+          doneButton.innerText = "done"
+          doneButton.classList.add("done-button")
+      }
 
-    // get title from object
-    const title = arrayOfTodos[index].title
+      doneButton.addEventListener('click', function () {
+          console.log("clicked")
+          li.style.color = "black"
+          li.style.textDecoration = "line-through"
+          li.removeChild(doneButton)
+      })
+  });
+  fetchTodos()   
+}
 
-    // put title in the li element
-    const listItemText = document.createTextNode(title);
-
-    // add textNode to list element
-    li.appendChild(listItemText)
-    // add list item to ol element in DOM
-    // document.body.insertBefore(li, ol);
-    ol.appendChild(li)
+const filterTodos = (filtered) => {
+  document.getElementById('todo-list').innerHTML = ""
+  populateTodos(filtered)
   }
+
+const isDone = (trueOrFalse) => {
+  let completed = trueOrFalse
+  filteredList = filteredList.filter(function (todo) {
+      if (todo.completed === completed) {
+          return todo
+      }
+  })
+
+  filterTodos(filteredList)
+  console.log(filteredList);
+}
+
+const idFilter = () => {
+  let userID = document.getElementById('userID').value;
+  console.log(userID)
+  filteredList = arrayOfTodos.filter(function (todo) {
+      if (todo.userId == userID) {
+          return todo
+      }
+  })
+  filterTodos(filteredList)
+  console.log(filteredList)
 }
